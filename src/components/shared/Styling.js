467f3cfd7e859c35140f2889f2b0bc22d0ddd1b0
Zene09 <zeneorr@gmail.com>
2022-08-29 15:13:16 -0500

    style buttons

---
+++ src/components/shared/Styling.js
@@ -0,0 +1,25 @@
+/*
+B9CEA1 = light green
+381D2A = dark purple
+BA5624 = darker orange
+F4A460 = Sandy Brown
+FEF8EB = off-white
+
+fontFamily: 'Play, sans-serif'
+*/
+
+// class="btn btn-outline-dark"
+// class="btn btn-outline-light"
+export const submitButton = {
+    color: '#381D2A',
+    backgroundColor: '#B9CEA1',
+    margin: '5px'
+}
+
+export const showButton = {
+    color: '#FEF8EB',
+    backgroundColor: '#BA5624',
+    margin: '5px'
+}
+
+export const 
